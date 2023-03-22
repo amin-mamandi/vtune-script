775079c857bcfba70d545389b0a4543582bd6db7
Amin Mamandipoor <a972m888@pollux.ittc.ku.edu>
2023-03-22 18:52:15 -0500

    added comments

--- run_script.sh
+++ run_script.sh
@@ -4,6 +4,7 @@ if [ "$#" -ne 1 ]; then
   echo "Usage: $0 <python_script>"
   exit 1
 fi
-
+# run the python script with the given argument from the outside world
+# rdtset limits the memory bandwith to 80% of its actual bandwidth, limiting the llc ways to 8 ways, this applies to 0-15 cores.  -c 0-15 sets the affinity and make the python script to run only on these cores 
 sudo rdtset -t 'mba=80;l3=0xff;cpu=0-15' -c 0-15 python "$1"
 
--- vtune-auto.sh
+++ vtune-auto.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-
-#find . -name '*.py' -not -name 'test.py' -type f -print0 | while IFS= read -r -d '' file; do
+find . -name '*.py' -not -name 'test.py' -type f -print0 | while IFS= read -r -d '' file; do
+  # make sure that we don't have unclosed queues + reseting any cpu masks
   rm /dev/mqueue/*
   pqos -R
-  killall -9 python
-  vtune -collect=uarch-exploration  /home/a972m888/Desktop/dmx/dmx-kernel-eval/scripts/run_script.sh multiple_runs_3gzip_2aes.py
-  #echo "$(basename "$file")"
-#done
+  #killall -9 python
+  
+  # start collecting uArch exploration for the targeted python script
+  vtune -collect=uarch-exploration /PATH/TO/run_script.sh "$(basename "$file")" 
+  
+  # exporting the collected results to a .csv file
+  vtune -report summary -r OUTPUT-DIR -report-output = "$(basename -s .py "$file")".csv -format=csv -csv-delimiter=comma –q 
+done
