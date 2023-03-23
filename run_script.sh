037ffc8e30603f0a0748e124b39f150c30080dce
Amin-Mamandi <61329349+amin-mamandi@users.noreply.github.com>
2023-03-22 21:27:20 -0500

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -5,6 +5,8 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 # run the python script with the given argument from the outside world
-# rdtset limits the memory bandwith to 80% of its actual bandwidth, limiting the llc ways to 8 ways, this applies to 0-15 cores.  -c 0-15 sets the affinity and make the python script to run only on these cores 
+# rdtset limits the memory bandwith to 80% of its actual bandwidth, limiting the llc ways to 8 ways 
+# also, this applies only to 0-15 cores  
+# -c 0-15 sets the affinity and make the python script to run only on these cores 
 sudo rdtset -t 'mba=80;l3=0xff;cpu=0-15' -c 0-15 python "$1"
 
